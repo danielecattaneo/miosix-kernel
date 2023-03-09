375fd5b210debdf3d863c45b08fff2c1fee05179
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2023-03-09 01:54:42 +0100

    DO NOT UPSTREAM top test code

--- main.cpp
+++ main.cpp
@@ -1,11 +1,32 @@
 
 #include <cstdio>
+#include <thread>
+#include <map>
 #include "miosix.h"
+#include "kernel/scheduler/scheduler.h"
 
 using namespace std;
 using namespace miosix;
 
+void spin()
+{
+    delayMs(1000*20);
+}
+
+void top()
+{
+    #ifdef WITH_CPU_TIME_COUNTER
+    CPUProfiler::thread(1000000000LL);
+    #endif
+}
+
 int main()
 {
-    //iprintf("Hello world, write your application here\n");
+    std::thread topThd(top);
+    std::thread spinThd(spin);
+    delayMs(1000*5);
+    for (;;) {
+        sleep(1);
+    }
 }
+
