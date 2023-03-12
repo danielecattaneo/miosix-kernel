8ee5d7c856845edb0b33973f8d45392624d5e9b5
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2023-03-12 23:27:34 +0100

    DO NOT UPSTREAM cmsis update test code

--- main.cpp
+++ main.cpp
@@ -7,5 +7,43 @@ using namespace miosix;
 
 int main()
 {
-    //iprintf("Hello world, write your application here\n");
+    for (;;) {
+        char c;
+        do {
+            iprintf("hello, type s\n");
+            scanf("%c\n", &c);
+        } while (c != 's');
+        iprintf("core clock is %ld\n", SystemCoreClock);
+        for (int i=0; i<10000; i+=100) {
+            iprintf("testing %d us\n", i);
+            long long delta;
+            {
+                FastInterruptDisableLock dLock;
+                auto start=IRQgetTime();
+                delayUs(i);
+                delta=IRQgetTime()-start;
+            }
+            iprintf("IRQgetTime=%lld delayUs=%d\n", delta, i);
+        }
+        for (int i=0; i<1000; i+=50) {
+            iprintf("testing %d ms\n", i);
+            long long delta;
+            {
+                FastInterruptDisableLock dLock;
+                auto start=IRQgetTime();
+                delayMs(i);
+                delta=IRQgetTime()-start;
+            }
+            iprintf("IRQgetTime=%lld delayMs=%d\n", delta, i);
+        }
+        do {
+            iprintf("type s and start timing 5 seconds\n");
+            scanf("%c\n", &c);
+        } while (c != 's');
+        {
+            FastInterruptDisableLock dLock;
+            delayUs(5000000);
+        }
+        iprintf("End of the 5 seconds\n");
+    }
 }
