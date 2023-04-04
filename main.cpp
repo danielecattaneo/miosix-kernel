dde9dc017441e9ea43990bd718019d4027ef0597
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2023-04-04 22:31:23 +0200

    DO NOT UPSTREAM test code

--- main.cpp
+++ main.cpp
@@ -10,7 +10,16 @@ using namespace miosix;
 
 void spin()
 {
+    #ifdef WITH_CPU_TIME_COUNTER
+    long long a = CPUTimeCounter::getActiveThreadTime();
+    iprintf("spin thread time 0 = %lld\n", a);
+    #endif
     delayMs(1000*20);
+    #ifdef WITH_CPU_TIME_COUNTER
+    long long b = CPUTimeCounter::getActiveThreadTime();
+    iprintf("spin thread time 1 = %lld\n", b);
+    iprintf("spin thread dt = %lld\n", b - a);
+    #endif
 }
 
 void top()
@@ -24,7 +33,16 @@ int main()
 {
     std::thread topThd(top);
     std::thread spinThd(spin);
+    #ifdef WITH_CPU_TIME_COUNTER
+    long long a = CPUTimeCounter::getActiveThreadTime();
+    iprintf("main thread time 0 = %lld\n", a);
+    #endif
     delayMs(1000*5);
+    #ifdef WITH_CPU_TIME_COUNTER
+    long long b = CPUTimeCounter::getActiveThreadTime();
+    iprintf("main thread time 1 = %lld\n", b);
+    iprintf("main thread dt = %lld\n", b - a);
+    #endif
     for (;;) {
         sleep(1);
     }
