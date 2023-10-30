4f767113682efb9cd491d9d92f4df2cd4e743b5f
Daniele Cattaneo <daniele3.cattaneo@mail.polimi.it>
2023-10-30 19:45:19 +0100

    Test code. DO NOT UPSTREAM

--- Makefile
+++ Makefile
@@ -18,6 +18,7 @@ SUBDIRS := $(KPATH)
 ##
 SRC :=                                  \
 main.cpp
+#miosix/_tools/testsuite/testsuite.cpp
 
 ##
 ## List here additional static libraries with relative path
--- main.cpp
+++ main.cpp
@@ -7,5 +7,30 @@ using namespace miosix;
 
 int main()
 {
-    //iprintf("Hello world, write your application here\n");
+    iprintf("marco-ram-board half-assed RAM test\n");
+    iprintf("pattern? 0: 55/AA, 1: shifting 1\n");
+    int pattern;
+    scanf("%d", &pattern);
+    volatile uint32_t *sdram_area = (uint32_t *)0xC0000000;
+    uint32_t i = 0;
+    uint32_t test;
+    if (pattern == 0)
+        test = 0x55555555;
+    else
+        test = 0x00000001;
+    while (true) {
+        sdram_area[i] = test;
+        iprintf("addr. %p wrote %08lx read %08lx\n", &sdram_area[i], test, sdram_area[i]);
+        i = (i + 0x4000401) & (0x1FFFFFFF / 4);
+        if ((i & 1) == 0) {
+            if (pattern == 0) {
+                test = ~test;
+            } else {
+                test = test << 1;
+                if (test == 0)
+                    test = 0x00000001;
+            }
+        }
+    }
 }
+
